3b2592f56565375c8c703eb6a00b4e54c4549199
yangfei <yangfei.me@gmail.com>
2012-11-04 23:46:08 +0800

    extract images from "/System/Library/PrivateFrameworks"

--- BorrowImage/ViewController.m
+++ BorrowImage/ViewController.m
@@ -8,16 +8,53 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define Target_Path @"/System/Library/PrivateFrameworks"
 
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property(nonatomic, strong)UITableView     *tableView;
+@property(nonatomic, strong)NSMutableArray  *imagePathArray;
 @end
 
 @implementation ViewController
+
+- (void)updateTitle:(NSString *)str{
+    self.title = str;
+}
+
+- (void)searchImages{
+    @autoreleasepool {
+        for (NSString *relativePath in [[NSFileManager defaultManager] enumeratorAtPath:Target_Path])
+        {
+            if ([relativePath hasSuffix:@".png"]) {
+                
+                NSArray *array = [relativePath componentsSeparatedByString:@"/"];
+                NSString *imageName = [array lastObject];
+                NSString *imagePath = [NSString stringWithFormat:@"%@/%@",Target_Path,relativePath];
+                NSDictionary *imageDict = [NSDictionary dictionaryWithObjectsAndKeys:imageName,@"ImageName",imagePath,@"ImagePath", nil];
+                [self.imagePathArray addObject:imageDict];
+                [self performSelectorOnMainThread:@selector(updateTitle:) withObject:[NSString stringWithFormat:@"(%d)%@",[self.imagePathArray count],relativePath] waitUntilDone:YES];
+            }
+        }
+        [self performSelectorOnMainThread:@selector(updateTitle:) withObject:[NSString stringWithFormat:@"(%d)%@",[self.imagePathArray count],Target_Path] waitUntilDone:YES];
+        [self.tableView reloadData];
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStyleGrouped];
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    
+    //NSString *currentPath = [[NSFileManager defaultManager] currentDirectoryPath];
+    //self.title = currentPath;
+    
+    self.imagePathArray = [NSMutableArray array];
+    [NSThread detachNewThreadSelector:@selector(searchImages) toTarget:self withObject:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +62,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.imagePathArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ContentsCellIdentifier = @"ContentsCell";
+    UITableViewCell *contentsCell = [tableView dequeueReusableCellWithIdentifier:ContentsCellIdentifier];
+    if (contentsCell == nil){
+        contentsCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ContentsCellIdentifier];
+    }
+    contentsCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    contentsCell.textLabel.text = [[self.imagePathArray objectAtIndex:indexPath.row] objectForKey:@"ImageName"];
+    contentsCell.detailTextLabel.text = [[self.imagePathArray objectAtIndex:indexPath.row] objectForKey:@"ImagePath"];
+    contentsCell.imageView.image = [UIImage imageWithContentsOfFile:[[self.imagePathArray objectAtIndex:indexPath.row] objectForKey:@"ImagePath"]];
+    contentsCell.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    return contentsCell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 
 @end
