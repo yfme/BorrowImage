2339e7415ed2c84f1d151c145e29e9ad3c5ac540
yangfei <yangfei.me@gmail.com>
2012-11-04 23:38:26 +0800

    Initial Commit

---
+++ BorrowImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BorrowImage
+//
+//  Created by Yang Fei on 12-11-4.
+//  Copyright (c) 2012年 能力有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BorrowImage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BorrowImage
+//
+//  Created by Yang Fei on 12-11-4.
+//  Copyright (c) 2012年 能力有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
